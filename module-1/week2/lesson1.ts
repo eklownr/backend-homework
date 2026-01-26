d50343a2d7f5d3758bef300d4cee013094661964
eklownr <eklownr@gmail.com>
2026-01-26 14:55:31 +0100

    1 hello callback

---
+++ module-1/week2/lesson1.ts
@@ -0,0 +1,15 @@
+type PrintHelloFunc = (message: string) => void;
+
+const hello = (s: string, saHello: PrintHelloFunc): void => {
+    console.log("runnig callback in 2 seconds");
+    
+    setTimeout(() => {
+        sayHello(s);
+    },2000);
+};
+
+const sayHello = (message: string): void => {
+    console.log(message);
+};
+
+hello("hello from callback", sayHello)
