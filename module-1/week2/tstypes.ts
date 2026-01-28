a5898bb56eaed20bdec94161662a136aa4046b5a
eklownr <eklownr@gmail.com>
2026-01-28 16:06:02 +0100

    Enum

--- module-1/week2/tstypes.ts
+++ module-1/week2/tstypes.ts
@@ -36,65 +36,6 @@ const myData = ({
 
 clientData(myData);
 
-/*
-// enum do not work without "node --loader ts-node/esm"
-// Error: TypeScript enum is not supported in strip-only mode
-enum Traficlight {
-    RED = "RED",
-    YELLOW = "YELLOW",
-    GREEN = "GREEN",
-}
-
-const lightAction = (light: Traficlight) => {
-    if (light === Traficlight.RED) {
-      console.log("Stop RED");
-  }
-    if (light === Traficlight.YELLOW) {
-      console.log("Stop YELLOW");
-  }
-    if (light === Traficlight.GREEN) {
-      console.log("Go GREEN");
-  }
-}
-
-lightAction(Traficlight.RED);
-
-*/
-
-
-// declaring enum as const object. Works with node
-const TrafficLight = {
-    RED: 'red',
-    YELLOW: 'yellow',
-    GREEN: 'green',
-} as const;
-
-// declaring type from enum and using keys: RED, YELLOW, GREEN 
-type TrafficLight = (typeof TrafficLight)[keyof typeof TrafficLight];
-
-const signal = (color: TrafficLight) => {
-    console.log(`Traffic light is ${color}`);
-        checkLight(color);
-}
-
-signal(TrafficLight.RED);    
-signal(TrafficLight.YELLOW);
-signal(TrafficLight.GREEN); 
- 
-
-// add function after enum and arrow-function-call to se if it works
-function checkLight(color: TrafficLight) {
-    if (color === TrafficLight.RED) {
-        console.log("Stop RED");
-    }
-    if (color === TrafficLight.YELLOW) {
-        console.log("Wait YELLOW ... ");
-    }
-    if (color === TrafficLight.GREEN) {
-        console.log("Go GREEN");
-    } 
- }
-
 
 /**********************************  
 Union types
@@ -208,9 +149,117 @@ Enum
 **********************************/
 // Exersice 1
 
+/*
+// enum do not work without "node --loader ts-node/esm"
+// Error: TypeScript enum is not supported in strip-only mode
+enum Traficlight {
+    RED = "RED",
+    YELLOW = "YELLOW",
+    GREEN = "GREEN",
+}
+
+const lightAction = (light: Traficlight) => {
+    if (light === Traficlight.RED) {
+      console.log("Stop RED");
+  }
+    if (light === Traficlight.YELLOW) {
+      console.log("Stop YELLOW");
+  }
+    if (light === Traficlight.GREEN) {
+      console.log("Go GREEN");
+  }
+}
+
+lightAction(Traficlight.RED);
+
+*/
+
+
+// declaring enum as const object. Works with node
+const TrafficLight = {
+    RED: 'red',
+    YELLOW: 'yellow',
+    GREEN: 'green',
+} as const;
+
+// declaring type from enum and using keys: RED, YELLOW, GREEN 
+type TrafficLight = (typeof TrafficLight)[keyof typeof TrafficLight];
+
+const signal = (color: TrafficLight) => {
+    console.log(`Traffic light is ${color}`);
+        checkLight(color);
+}
+
+signal(TrafficLight.RED);    
+signal(TrafficLight.YELLOW);
+signal(TrafficLight.GREEN); 
+ 
+
+// add function after enum and arrow-function-call to se if it works
+function checkLight(color: TrafficLight) {
+    if (color === TrafficLight.RED) {
+        console.log("Stop RED");
+    }
+    if (color === TrafficLight.YELLOW) {
+        console.log("Wait YELLOW ... ");
+    }
+    if (color === TrafficLight.GREEN) {
+        console.log("Go GREEN");
+    } 
+ }
+
+
+
 // Exersice 2
+// Create an enum PizzaSize with values: Small, Medium, Large. 
+// Write a function orderPizza that prints: 
+// "You ordered a [size] pizza." 
+const PizzaSize = {
+    SMALL: "Small",
+    MEDIUM: "Medium",
+    LARGE: "Large",
+} as const;
+
+// declaring type from enum and using keys: SMALL, MEDIUM, LARGE
+type PizzaSize = (typeof PizzaSize)[keyof typeof PizzaSize];
+
+const orderPizza = (size: PizzaSize) => {
+    console.log(`You ordered a ${size} pizza.`);
+}
+
+orderPizza(PizzaSize.SMALL);
+orderPizza(PizzaSize.MEDIUM);
+orderPizza(PizzaSize.LARGE);
 
 // Exersice 3
+// Create an enum Role with values: Admin, User, Guest. 
+// Write a function printRole that checks the role: 
+// Admin → "You have full access" 
+// User → "You have limited access" 
+// Guest → "You have guest access" 
+const Role = {
+    ADMIN: "Admin",
+    USER: "User",
+    GUEST: "Guest",
+} as const;
+
+type Role = (typeof Role)[keyof typeof Role];
+
+const printRoll = (roll: Role) => {
+  if (roll === Role.ADMIN) {
+    console.log(roll, "You have full access");
+  }
+  if (roll === Role.USER) {
+    console.log(roll, "You have limited access");
+  }
+  if (roll === Role.GUEST) {
+    console.log(roll, "You have guest access");
+  }
+}
+
+printRoll(Role.ADMIN)
+printRoll(Role.GUEST)
+printRoll(Role.USER)
 
 
 /**********************************  
