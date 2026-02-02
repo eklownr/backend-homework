86fe031ef042565211f259508fa031b90ca50f5f
eklownr <eklownr@gmail.com>
2026-02-02 12:54:49 +0100

    challenge 6

--- module-1/callback-challengers/challengers.ts
+++ module-1/callback-challengers/challengers.ts
@@ -120,31 +120,55 @@ console.log(mapWith([1, 2, 3], addTwo)); // should output [ 3, 4, 5 ]
 console.log(mapWith([5, 7, 9], addTwo)); // should output [ 7, 9, 11 ]
 
 // ________________________________________________________________________________________________
-// Challenge 6
+// Challenge 6 ✅
 /*
 The function reduce takes an array and reduces the elements to a single value.
 For example it can sum all the numbers, multiply them,
 or any operation that you can put into a function.
 */
+const reduce = (
+	arr: number[],
+	callback: (a: number, b: number) => number,
+	initial: number,
+): number => {
+	// return a number
+	let result = initial;
+	forEach(arr, (item) => {
+		result = callback(result, item);
+	});
+	return result;
+};
 
 const nums = [4, 1, 3];
-const add = function (a, b) {
+const add = function (a: number, b: number): number {
 	return a + b;
 };
+const mult = function (a: number, b: number): number {
+	return a * b;
+};
+const sub = function (a: number, b: number): number {
+	return a - b;
+};
+const div = function (a: number, b: number): number {
+	return a / b;
+};
 console.log(reduce(nums, add, 0));
+console.log(reduce(nums, mult, 1));
+console.log(reduce(nums, sub, 10));
+console.log(reduce(nums, div, 1));
 
-//should output 8
+//_______________________________________________________________________________________________
+//  Challenge 7
+/*  Construct a function intersection that compares input arrays 
+    and returns a new array with elements found in all of the inputs. BONUS: Use reduce!
+ */
+
+console.log(
+	intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]),
+);
+// should log: [5, 15]
 
 // ________________________________________________________________________________________________
-//  // Challenge 7
-//  /* Construct a function intersection that compares input arrays and returns a new array with elements found in all of the inputs. BONUS: Use reduce!
-//   */
-//  console.log(
-//    intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
-//  );
-//  // should log: [5, 15]
-//
-//  // ________________________________________________________________________________________________
 //  // Challenge 8
 //  /*
 //  Construct a function union that compares input arrays and returns a new array that contains all elements. If there are duplicate elements, only add it once to the new array. Preserve the order of the elements starting from the first element of the first input array. BONUS: Use reduce!
