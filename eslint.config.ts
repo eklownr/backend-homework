e2f543e471c07298b8ee67e435dd2e2536c74c80
eklownr <eklownr@gmail.com>
2026-02-09 15:26:17 +0100

    week4,  eslintLesson.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,6 +4,18 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.node } 
+  },
   tseslint.configs.recommended,
+  {
+		rules: {
+			"@typescript-eslint/no-unused-vars": "warn",
+			"no-undef": "warn",
+      "prefer-const": "warn",
+      "arrow-body-style": ["error", "always"],
+		},
+	},
 ]);
---
+++ module-1/week7/eslintLesson.ts
@@ -0,0 +1,12 @@
+const greet = (firstName: string) => {return 'Hello ' + firstName} 
+greet('Student') 
+
+const i:number = 0
+
+//test
+const testing = () => {return console.log('test')}
+testing()
+
+const printName = (name: string) => {
+    return console.log(name)
+}
