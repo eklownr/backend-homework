077a7175b8b916a82140b15e7c058051b6fba4cb
eklownr <eklownr@gmail.com>
2026-02-14 09:28:46 +0100

    update database

--- module-1/todoApp/todo.ts
+++ module-1/todoApp/todo.ts
@@ -1,5 +1,5 @@
 import * as readline from 'readline';
-import {Color, color, formatDate} from './formatUtils.js'
+import { Color, color, formatDate } from './formatUtils.ts'
 import * as fs from 'fs';
 
 
@@ -14,19 +14,27 @@ import * as fs from 'fs';
 
 // create type TodoPost
 type TodoPost = {
-    id?: number;
-    todo: string;
-    date: string; 
-    done?: Done;
-}; 
+  id?: number;
+  todo: string;
+  date: string;
+  done?: Done;
+};
 
 
 // enum for checking if task is done
-enum Done {
+const Done = {
+  DONE: "completed",
+  ACTIVE: "active",
+} as const;
+
+type Done = (typeof Done)[keyof typeof Done];
+
+/*
+pre-incrementum Done {
   DONE = "completed",
   ACTIVE = "active",
 }   
-
+*/
 
 // Globals and constants
 const filePath = './module-1/todoApp/todoDB.json';
@@ -37,219 +45,219 @@ let todoStorage: TodoPost[] = [];
 //*******************************
 // Read data from json and save it to todoStorage
 if (fs.existsSync(filePath)) {
-    const data = fs.readFileSync(filePath, 'utf-8');
-    todoStorage = data ? JSON.parse(data) : [];
-    // get the highest ID
-    todoStorage.forEach((post: TodoPost) => {
-        if (post.id) {
-            ID = Math.max(ID, post.id); // make sure ID is the highest
-        }
-    })
+  const data = fs.readFileSync(filePath, 'utf-8');
+  todoStorage = data ? JSON.parse(data) : [];
+  // get the highest ID
+  todoStorage.forEach((post: TodoPost) => {
+    if (post.id) {
+      ID = Math.max(ID, post.id); // make sure ID is the highest
+    }
+  })
 } else { // if file does not exist, create an empty one
   fs.writeFileSync(filePath, JSON.stringify([], null, 4), 'utf-8');
-}   
+}
 
 
 //*******************************
 // interface for input and output
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: true,
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true,
 });
 
 
 // event on close. Save todoStore-data to <todoDB>.json-file
 rl.on('close', () => {
-    console.log(color("green",'== Closing TODO App! =='));
-    // save to json
-    fs.writeFileSync(filePath, JSON.stringify(todoStorage, null, 4), 'utf-8');
-    process.exit(0);
-});   
+  console.log(color("green", '== Closing TODO App! =='));
+  // save to json
+  fs.writeFileSync(filePath, JSON.stringify(todoStorage, null, 4), 'utf-8');
+  process.exit(0);
+});
 
 
 //*******************************
 // Add new post to storage
 const add = (post: TodoPost): void => {
-    if (post) {
-        if (!post.id) {
-            post.id = ++ID; // pre-increment to avoid duplicate
-        } else {
-            ID = Math.max(ID, post.id); // make sure ID is the highest
-        }
-        post.done = Done.ACTIVE;
-        todoStorage.push(post);
-        console.log("Add post: ", post.id, post.todo, post.date);
+  if (post) {
+    if (!post.id) {
+      post.id = ++ID; // pre-increment to avoid duplicate
     } else {
-        console.log("Post is empty");
+      ID = Math.max(ID, post.id); // make sure ID is the highest
     }
-};   
+    post.done = Done.ACTIVE;
+    todoStorage.push(post);
+    console.log("Add post: ", post.id, post.todo, post.date);
+  } else {
+    console.log("Post is empty");
+  }
+};
 
 
 //*******************************
 // Ask to add new todo post
 function askToAddNewPost(): void {
-    console.log(color("yellow", "*******************************"));
-    rl.question(color("orange", "==> ") + " Add todo (or 'q' to quit): ", (input: string) => {
-        const answer = input.trim();
-        if (answer.toLowerCase() === "q") {
-            console.log(color("magenta", "Go back to main"));
-            message();
-            main(); // run main again
-            return;
-        }
-        if (answer) {
-            const newPost: TodoPost = {
-            todo: answer,
-            date: formatDate(new Date()),
-            };
-            add(newPost);
-        } else {
-            console.log("Empty input, try again.");
-        }
-        askToAddNewPost(); // ask again, recursive call instead of loop.
+  console.log(color("yellow", "*******************************"));
+  rl.question(color("orange", "==> ") + " Add todo (or 'q' to quit): ", (input: string) => {
+    const answer = input.trim();
+    if (answer.toLowerCase() === "q") {
+      console.log(color("magenta", "Go back to main"));
+      message();
+      main(); // run main again
+      return;
+    }
+    if (answer) {
+      const newPost: TodoPost = {
+        todo: answer,
+        date: formatDate(new Date()),
+      };
+      add(newPost);
+    } else {
+      console.log("Empty input, try again.");
+    }
+    askToAddNewPost(); // ask again, recursive call instead of loop.
   });
 };
 
- 
+
 //*******************************
 // Remove post by ID 
 const removePostById = (id: number): void => {
-    const index = todoStorage.findIndex(post => post.id === id);
-    if (index !== -1) {
-        todoStorage.splice(index, 1);
-        console.log(Color.RED + `Post with id ${id} has been removed.` + Color.RESET);
-    } else {
-        console.log(Color.YELLOW + `No post found with id ${id}.` + Color.RESET);
-    }
-};   
+  const index = todoStorage.findIndex(post => post.id === id);
+  if (index !== -1) {
+    todoStorage.splice(index, 1);
+    console.log(Color.RED + `Post with id ${id} has been removed.` + Color.RESET);
+  } else {
+    console.log(Color.YELLOW + `No post found with id ${id}.` + Color.RESET);
+  }
+};
 
 
 //*******************************
 // Ask to remove post
 function askToRemovePost() {
-    console.log(color("red", "*******************************"));
-    rl.question(color("orange", "==> Delete by ID number: ") + "('q' to quit or 'delall'): ", (input: string) => {
+  console.log(color("red", "*******************************"));
+  rl.question(color("orange", "==> Delete by ID number: ") + "('q' to quit or 'delall'): ", (input: string) => {
     const trimmed = input.trim().toLowerCase();
-        if (trimmed === "q") {
-            console.log(color("magenta", "Go back to main"));
-            message();
-            main(); // run main again
-            return;
-        }
-        if (trimmed === "delall") {
-            todoStorage.splice(0, todoStorage.length);
-            console.log(Color.RED + `All post/task has been removed.` + Color.RESET);
-            listAll();
-            return;
-        }
-        else if (trimmed) {
-           const id = parseInt(trimmed);
-           removePostById(id);
-           listAll();
-       }
-    });
+    if (trimmed === "q") {
+      console.log(color("magenta", "Go back to main"));
+      message();
+      main(); // run main again
+      return;
+    }
+    if (trimmed === "delall") {
+      todoStorage.splice(0, todoStorage.length);
+      console.log(Color.RED + `All post/task has been removed.` + Color.RESET);
+      listAll();
+      return;
+    }
+    else if (trimmed) {
+      const id = parseInt(trimmed);
+      removePostById(id);
+      listAll();
+    }
+  });
 }
 
 
 //*******************************
 // Mark post as done by ID 
 const markAsDone = (id: number): void => {
-    const post = todoStorage.find(p => p.id === id);
-        if (post) { 
-            if (post.done === Done.DONE) {
-                post.done = Done.ACTIVE;
-            } else { 
-                post.done = Done.DONE;
-            }; 
-            console.log(Color.GREEN + `Post with id ${id} is ${post.done}!`+ Color.RESET);
-        } else {
-        console.log(Color.YELLOW + `No post found with id ${id}.` + Color.RESET);
-        }
-};   
+  const post = todoStorage.find(p => p.id === id);
+  if (post) {
+    if (post.done === Done.DONE) {
+      post.done = Done.ACTIVE;
+    } else {
+      post.done = Done.DONE;
+    };
+    console.log(Color.GREEN + `Post with id ${id} is ${post.done}!` + Color.RESET);
+  } else {
+    console.log(Color.YELLOW + `No post found with id ${id}.` + Color.RESET);
+  }
+};
 
 
 //*******************************
 // Ask to set post as done
 function askToSetAsDone() {
-    console.log(color("blue", "*******************************"));
-    rl.question(color("orange", "==> Set as done/active, by ID number: ") + "( or 'q' to quit): ", (input: string) => {
+  console.log(color("blue", "*******************************"));
+  rl.question(color("orange", "==> Set as done/active, by ID number: ") + "( or 'q' to quit): ", (input: string) => {
     const trimmed = input.trim().toLowerCase();
-        if (trimmed === "q") {
-            console.log(color("magenta", "Go back to main"));
-            message();
-            main(); // run main again
-            return;
-        }
-        if (trimmed) {
-            const id = parseInt(trimmed);
-            markAsDone(id);
-            listAll();
-        }
-    });
+    if (trimmed === "q") {
+      console.log(color("magenta", "Go back to main"));
+      message();
+      main(); // run main again
+      return;
+    }
+    if (trimmed) {
+      const id = parseInt(trimmed);
+      markAsDone(id);
+      listAll();
+    }
+  });
 }
 
 
 //*******************************
 // Welcome message
 function message() {
-    console.log(color("cyan", "************************************'"));
-    console.log(color("green", "== Welcome to ToDo App! =="));
-    console.log("q - quit");
-    console.log("a - add new post");
-    console.log("l - list all post as a table");
-    console.log("d - delete post");
-    console.log("c - clear screen");
-    console.log("s - set post as done/active");
-    console.log("m - menu message");
-    console.log(color("cyan", "************************************'"));
+  console.log(color("cyan", "************************************'"));
+  console.log(color("green", "== Welcome to ToDo App! =="));
+  console.log("q - quit");
+  console.log("a - add new post");
+  console.log("l - list all post as a table");
+  console.log("d - delete post");
+  console.log("c - clear screen");
+  console.log("s - set post as done/active");
+  console.log("m - menu message");
+  console.log(color("cyan", "************************************'"));
 }
 
 
 //*******************************
 // list all todo-post
 function listAll(): void {
-    console.table(todoStorage);
-    main(); // run main again
+  console.table(todoStorage);
+  main(); // run main again
 }
 
 
 //*****************************************//
 // main function takes care of user input  //
 //*****************************************//
-function main() {    
-    rl.question(color("blue", "==> ") +"What do you want to do? ", (input: string) => {
-        const trimmed = input.trim().toLowerCase();
-        if (trimmed === "q") {
-            console.log(color("magenta","Bye ..."));
-            rl.close();
-            return;
-        }
-        else if (trimmed === "a") {
-            askToAddNewPost();   
-        }
-        else if (trimmed === "l") {
-            listAll();   
-        }
-        else if (trimmed === "m") {
-            message();   
-            main();
-        }
-        else if (trimmed === "s") {
-            askToSetAsDone();   
-        }
-        else if (trimmed === "c") {
-            console.clear();
-            message();   
-            main();
-        }
-        else if (trimmed === "d") {
-            askToRemovePost();   
-        }else {
-            console.log(color("orange","Wrong input, try again."));
-            main()
-        }
-    });
+function main() {
+  rl.question(color("blue", "==> ") + "What do you want to do? ", (input: string) => {
+    const trimmed = input.trim().toLowerCase();
+    if (trimmed === "q") {
+      console.log(color("magenta", "Bye ..."));
+      rl.close();
+      return;
+    }
+    else if (trimmed === "a") {
+      askToAddNewPost();
+    }
+    else if (trimmed === "l") {
+      listAll();
+    }
+    else if (trimmed === "m") {
+      message();
+      main();
+    }
+    else if (trimmed === "s") {
+      askToSetAsDone();
+    }
+    else if (trimmed === "c") {
+      console.clear();
+      message();
+      main();
+    }
+    else if (trimmed === "d") {
+      askToRemovePost();
+    } else {
+      console.log(color("orange", "Wrong input, try again."));
+      main()
+    }
+  });
 }
 
 // welcome message
